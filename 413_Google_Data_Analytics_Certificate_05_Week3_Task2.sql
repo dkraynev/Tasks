0e5cb5664bc8770724c808d49b0642487a48f9ba
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-10-02 16:53:29 +0300

    Add files via upload

--- 413_Google_Data_Analytics_Certificate_05_Week3_Task2.sql
+++ 413_Google_Data_Analytics_Certificate_05_Week3_Task2.sql
@@ -1,3 +1,6 @@
+-- JOIN operator
+
+-- #1
 SELECT
   O.*,
   W.warehouse_alias,
@@ -8,10 +11,44 @@ FROM
     `inlaid-stratum-383516.warehouse_orders.warehouse` W
   ON O.warehouse_id = W.warehouse_id;
 
+
+-- #2
 SELECT
   COUNt (DISTINCT W.state)
 FROM
   `inlaid-stratum-383516.warehouse_orders.orders` O
   JOIN
     `inlaid-stratum-383516.warehouse_orders.warehouse` W
-  ON O.warehouse_id = W.warehouse_id
+  ON O.warehouse_id = W.warehouse_id;
+
+-- #3
+SELECT
+ station_id,
+ num_bikes_available,
+ (SELECT
+  AVG(num_bikes_available)
+ FROM `bigquery-public-data.new_york_citibike.citibike_stations`
+ ) AS avg_num_bike_avalable
+FROM
+  `bigquery-public-data.new_york_citibike.citibike_stations`
+ORDER BY
+  2 DESC;
+
+
+-- #4
+SELECT
+  station_id,
+  name,
+  number_of_rides AS number_of_rides_starting_at_station
+FROM 
+  (
+  SELECT
+    CAST(start_station_id AS STRING) AS start_station_id_str,
+    COUNT(*) AS number_of_rides
+  FROM `bigquery-public-data.new_york.citibike_trips`
+  GROUP BY CAST(start_station_id AS STRING)
+  ) AS station_num_trips
+  INNER JOIN
+    `bigquery-public-data.new_york.citibike_stations`
+  ON station_id = start_station_id_str
+ORDER BY number_of_rides DESC;
