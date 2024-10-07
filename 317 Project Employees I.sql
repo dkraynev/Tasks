646283b2cdaebd0d6923a8847913347f9abee39e
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-10-07 22:39:17 +0300

    Add files via upload

--- 317 Project Employees I.sql
+++ 317 Project Employees I.sql
@@ -1,67 +1,77 @@
-Table: Project
+TTable: Users
 
 +-------------+---------+
 | Column Name | Type    |
 +-------------+---------+
-| project_id  | int     |
-| employee_id | int     |
+| user_id     | int     |
+| user_name   | varchar |
 +-------------+---------+
-(project_id, employee_id) is the primary key of this table.
-employee_id is a foreign key to Employee table.
-Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+user_id is the primary key (column with unique values) for this table.
+Each row of this table contains the name and the id of a user.
  
 
-Table: Employee
+Table: Register
 
-+------------------+---------+
-| Column Name      | Type    |
-+------------------+---------+
-| employee_id      | int     |
-| name             | varchar |
-| experience_years | int     |
-+------------------+---------+
-employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
-Each row of this table contains information about one employee.
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| contest_id  | int     |
+| user_id     | int     |
++-------------+---------+
+(contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id of a user and the contest they registered into.
  
 
-Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
 
-Return the result table in any order.
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
 
-The query result format is in the following example.
+The result format is in the following example.
 
  
 
 Example 1:
 
 Input: 
-Project table:
-+-------------+-------------+
-| project_id  | employee_id |
-+-------------+-------------+
-| 1           | 1           |
-| 1           | 2           |
-| 1           | 3           |
-| 2           | 1           |
-| 2           | 4           |
-+-------------+-------------+
-Employee table:
-+-------------+--------+------------------+
-| employee_id | name   | experience_years |
-+-------------+--------+------------------+
-| 1           | Khaled | 3                |
-| 2           | Ali    | 2                |
-| 3           | John   | 1                |
-| 4           | Doe    | 2                |
-+-------------+--------+------------------+
+Users table:
++---------+-----------+
+| user_id | user_name |
++---------+-----------+
+| 6       | Alice     |
+| 2       | Bob       |
+| 7       | Alex      |
++---------+-----------+
+Register table:
++------------+---------+
+| contest_id | user_id |
++------------+---------+
+| 215        | 6       |
+| 209        | 2       |
+| 208        | 2       |
+| 210        | 6       |
+| 208        | 6       |
+| 209        | 7       |
+| 209        | 6       |
+| 215        | 7       |
+| 208        | 7       |
+| 210        | 2       |
+| 207        | 2       |
+| 210        | 7       |
++------------+---------+
 Output: 
-+-------------+---------------+
-| project_id  | average_years |
-+-------------+---------------+
-| 1           | 2.00          |
-| 2           | 2.50          |
-+-------------+---------------+
-Explanation: The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
++------------+------------+
+| contest_id | percentage |
++------------+------------+
+| 208        | 100.0      |
+| 209        | 100.0      |
+| 210        | 100.0      |
+| 215        | 66.67      |
+| 207        | 33.33      |
++------------+------------+
+Explanation: 
+All the users registered in contests 208, 209, and 210. The percentage is 100% and we sort them in the answer table by contest_id in ascending order.
+Alice and Alex registered in contest 215 and the percentage is ((2/3) * 100) = 66.67%
+Bob registered in contest 207 and the percentage is ((1/3) * 100) = 33.33%
 
 -- Option #1
 # Write your MySQL query statement below
