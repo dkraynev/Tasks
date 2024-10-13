5e30de4ea5bb0108b65ab917f30bcb96dd439394
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-10-13 11:37:38 +0300

    Add files via upload

--- 319 Queries Quality and Percentage.sql
+++ 319 Queries Quality and Percentage.sql
@@ -84,3 +84,20 @@ WHERE
     NOT Q1.query_name IS NULL
 GROUP BY
     1;
+
+-- option #2
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2)   AS quality,
+    ROUND(100*SUM(
+        CASE
+            WHEN
+                rating < 3
+            THEN 1
+            ELSE 0
+        END)
+        / COUNT(rating), 2) AS poor_query_percentage
+FROM
+    Queries
+WHERE NOT query_name IS NULL
+GROUP BY 1;
