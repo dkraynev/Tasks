5f1f4b05b2e9353457af8803cb0d11c523251150
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-10-16 17:52:20 +0300

    Add files via upload

--- 329 Fix Names in a Table.sql
+++ 329 Fix Names in a Table.sql
@@ -96,3 +96,17 @@ SELECT
 FROM
     Users
 ORDER BY user_id;
+
+
+-- Option #3
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(SUBSTRING(name, 1, 1)),
+        LOWER(SUBSTRING(name, 2))
+    ) AS name
+
+FROM
+    Users
+ORDER BY
+    user_id;
