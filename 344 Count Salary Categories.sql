0b49199353e7576fc4f648a07802ebcfa8666c76
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-10-18 14:12:43 +0300

    Add files via upload

--- 344 Count Salary Categories.sql
+++ 344 Count Salary Categories.sql
@@ -179,3 +179,24 @@ SELECT
     SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS accounts_count
 FROM 
     Accounts
+
+-- Option #5
+SELECT
+    'Low Salary' AS category,
+    SUM(income < 20000) AS accounts_count
+FROM
+    Accounts
+
+UNION
+SELECT
+    'Average Salary' AS category,
+    SUM(income >= 20000 AND income <= 50000) AS accounts_count
+FROM
+    Accounts
+
+UNION
+SELECT
+    'High Salary' AS category,
+    SUM(income > 50000) AS accounts_count
+FROM
+    Accounts
