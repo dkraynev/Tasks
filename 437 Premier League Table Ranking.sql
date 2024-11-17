4b0934201379b0b27b11813d5ed8cd5279d41a07
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-11-17 13:05:07 +0200

    Add files via upload

---
+++ 437 Premier League Table Ranking.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+WITH TeamStats2 AS(
+SELECT
+    team_id,
+    team_name,
+    SUM(wins * 3 + draws * 1) AS points
+FROM
+    TeamStats
+GROUP BY
+    team_id, team_name
+)
+
+SELECT
+    team_id,
+    team_name,
+    points,
+    RANK() OVER(ORDER BY points DESC) AS position
+FROM
+    TeamStats2
+ORDER BY
+    points DESC,
+    team_name ASC;
