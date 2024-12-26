c2580a77d5e44fd7a9cc1370fbc78333f7285445
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-12-26 11:43:09 +0200

    Add files via upload

---
+++ 703-3 Customers Who Never Order. MySQL.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    name AS Customers
+FROM
+    Customers C
+    LEFT JOIN
+        Orders O
+    ON C.id = O.customerId
+WHERE
+    customerId IS NULL;
