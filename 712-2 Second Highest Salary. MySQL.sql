e0ca7cfd3ad8d45c62b3318f36779afd559681b5
dkraynev <89610013+dkraynev@users.noreply.github.com>
2024-12-29 07:21:44 +0200

    Add files via upload

---
+++ 712-2 Second Highest Salary. MySQL.sql
@@ -0,0 +1,13 @@
+SELECT
+    IFNULL(
+        (SELECT
+            DISTINCT salary
+        FROM
+            Employee
+        ORDER BY
+            salary DESC
+        LIMIT 1
+        OFFSET 1
+        ),
+        NULL
+    ) AS SecondHighestSalary;
